c85a2e4aad13b4b7849ad84b5fbae04352e2d10f
Security <91418733+Rikosetas@users.noreply.github.com>
2026-03-21 23:28:59 +0200

    pridetas tyrimui 3 bandymai is viso

--- ConsoleApplication1/testavimas.cpp
+++ ConsoleApplication1/testavimas.cpp
@@ -154,6 +154,7 @@ void tyrimas1_failuKurimas( )
 
         for ( int b = 0; b < bandymu_sk; b++ )
         {
+
             std::remove( pavadinimai[ i ].c_str( ) );
 
             auto start = std::chrono::high_resolution_clock::now( );
